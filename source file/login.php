24298fd213f9dd1e3d6d28490c856e440f6f60eb
Buthmika <buthmikamenushan@gmail.com>
2024-06-02 11:34:57 +0530

    Update login.php

--- source file/login.php
+++ source file/login.php
@@ -1,9 +1,9 @@
 <?php
-if(isset($_POST["submit"]))
-{
-
+if (isset($_POST["submit"])) {
+    // Form handling logic here
+    // Example: process form data, save to database, etc.
+} else {
+    header('Location: /login.php');
+    exit(); // Ensures the script stops executing after the redirect
 }
-else
-{
-    header('Location:/login.php');
-}
+?>
